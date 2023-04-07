03a6fad76b9578b5b0cfdb4a6efbb87840598f83
tad34s <tadeas.baca@email.cz>
2023-04-07 17:56:02 +0200

    improved preset network

--- bot/Networks.py
+++ bot/Networks.py
@@ -10,31 +10,35 @@ class CNNetwork_preset(torch.nn.Module):
         super(CNNetwork_preset, self).__init__()
         self.conv_5 = nn.Conv2d(2,8,kernel_size=5, stride=1, padding=2)
         self.conv_5.weight = torch.nn.Parameter(self.create_weights(5))
-        self.conv_train1 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
-        self.conv_train2 = nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1)
-        self.linear = nn.Linear(1024,  self.size ** 2)
+        self.conv_5.bias = torch.nn.Parameter(torch.tensor([0,0,0,0,0,0,0,0],dtype=torch.float32))
+        self.conv_friendly = nn.Conv2d(4,1,kernel_size=3, stride=1, padding=1)
+        self.conv_enemy = nn.Conv2d(4,1,kernel_size=3, stride=1, padding=1)
+        self.linear1 = nn.Linear(512,256)
+        self.linear2 = nn.Linear(128,  self.size ** 2)
         self.conv_activation = nn.ReLU()
         self.activation = nn.Tanh()
-        self.softmax = nn.Softmax(-1)
+        self.softmax = nn.Softmax()
 
     def forward(self, x):
         self.eval()
         with torch.no_grad():
             x = self.conv_5(x)
-            x = self.conv_activation(x)
-        x = self.conv_train1(x)
-        x = self.conv_activation(x)
-        x = self.conv_train2(x)
-        x = x.flatten(start_dim=1)
-        self.activation = nn.Tanh()
-        q_values = self.linear(x)
+        friendly,enemy = torch.split(x[0],4)
+        friendly = self.conv_friendly(friendly)
+        friendly = self.conv_activation(friendly)
+        enemy = self.conv_enemy(enemy)
+        enemy = self.conv_activation(enemy)
+        x = torch.cat((friendly,enemy))
+        x = x.flatten()
+        x = self.activation(x)
+        q_values = self.linear2(x)
         return q_values
 
     def probs(self, x):
         self.eval()
         with torch.no_grad():
             q_values = self.forward(x)
-            return self.softmax(q_values)[0], q_values[0]
+            return self.softmax(q_values), q_values
 
     def create_weights(self,kernel_size):
         # creates 8 kernel weights first 4 is 2 diagonals, row and column the second 4 are exact same but checks the enemy
